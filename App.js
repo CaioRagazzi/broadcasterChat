2c1af1aadce54dd7bf33f560b2357d50b0bd9764
Caio <ca.ragazzi@gmail.com>
2020-04-18 13:32:22 -0300

    inclusao do firebase

--- App.js
+++ App.js
@@ -1,114 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  TextInput,
+  Button,
+  TouchableOpacity,
   View,
-  Text,
-  StatusBar,
+  Text
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import firestore from '@react-native-firebase/firestore';
 
 const App = () => {
+
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState(null)
+
+  useEffect(() => {
+    const subscriber = firestore()
+      .collection('chats')
+      .onSnapshot(onResult, onError);
+    return () => subscriber();
+  }, [])
+
+  function onResult(collectionSnapshot) {
+    // setMessages(collectionSnapshot)
+    console.log(collectionSnapshot.forEach(element => {
+      console.log(element);
+      
+    }))
+  }
+
+  function onError(error) {
+    console.error(error);
+  }
+
+  function createMessage() {
+    firestore()
+      .collection('chats')
+      .doc()
+      .set({
+        author: 'caio',
+        message
+      })
+  }
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
+      {/* <StatusBar barStyle="dark-content" /> */}
+      <SafeAreaView style={{ flexGrow: 1 }}>
+        <ScrollView contentContainerStyle={styles.container}
+          contentInsetAdjustmentBehavior="automatic">
+
         </ScrollView>
+        <View style={styles.mensagemContainer}>
+          <TextInput
+            style={{ height: 40, width: '80%', borderColor: 'gray', borderWidth: 1 }}
+            onChangeText={text => setMessage(text)}
+            value={message}
+          />
+          <TouchableOpacity
+            style={styles.customButton}
+            onPress={() => { createMessage() }}
+            accessibilityLabel="Send"
+          >
+            <Text style={{ color: 'white' }}>Send</Text>
+          </TouchableOpacity>
+        </View>
       </SafeAreaView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  mensagemContainer: {
+    flexDirection: 'row'
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  customButton: {
+    width: '20%',
+    height: '100%',
+    backgroundColor: "#841584",
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 40
+  }
 });
 
 export default App;
